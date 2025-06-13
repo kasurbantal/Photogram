71a4a365e606addb0ea30bf5a564d419dd259075
kasurbantal <kasurbantal880@gmail.com>
2025-06-13 21:59:37 +0700

    feat: set sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,9 +1,33 @@
+import * as React from "react";
+import HomeIcon from "@/assets/icons/home.svg";
+import { Link } from "react-router-dom";
+
 interface ISidebarProps {}
+
+const navItems = [
+  {
+    name: "Home",
+    link: "/",
+    icon: HomeIcon,
+  },
+];
 
 const Sidebar: React.FunctionComponent<ISidebarProps> = () => {
   return (
     <>
-      <div className="flex bg-white">Sidebar</div>
+      <nav className="flex flex-col space-x-2 relative h-screen max-w-sm w-full">
+        <div className="flex justify-center m-5">
+          <div className="text-white text-lg">PhotoGram</div>
+        </div>
+        {navItems.map((item) => (
+          <div className="" key={item.name}>
+            <Link to={item.link}>
+              <span>{item.icon}</span>
+              <span>{item.name}</span>
+            </Link>
+          </div>
+        ))}
+      </nav>
     </>
   );
 };
