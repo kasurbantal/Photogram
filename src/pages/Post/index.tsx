27774bd48684717f71496037058081c4a555b71a
kasurbantal <kasurbantal880@gmail.com>
2025-06-20 23:02:19 +0700

    feat: set up label in post page

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,4 +1,5 @@
 import Layout from "@/components/layout";
+import { Label } from "@/components/ui/label";
 import * as React from "react";
 
 interface ICreatePostProps {}
@@ -7,7 +8,22 @@ const CreatePost: React.FunctionComponent<ICreatePostProps> = () => {
   return (
     <>
       <Layout>
-        <div>CreatePost</div>
+        <div className="flex justify-center">
+          <div className="border max-w-3xl w-full">
+            <h3 className="bg-slate-800 text-white text-center text=lg p-2">
+              First Post
+            </h3>
+            <div className="p-8 ">
+              <form action="">
+                <div className="flex flex-col">
+                  <Label className="mb-4" htmlFor="caption">
+                    Photo Caption
+                  </Label>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </Layout>
     </>
   );
